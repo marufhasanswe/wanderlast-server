22b2b9a5cec0f52941de056de3c7e21cd54abddb
marufhasanswe <marufhasanswe@gmail.com>
2026-05-20 21:37:01 +0600

    Server-side authorization with Better auth JWT token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
@@ -18,6 +19,28 @@ const client = new MongoClient(uri, {
   },
 });
 
+const JWKS = createRemoteJWKSet(new URL("http://localhost:3000/api/auth/jwks"));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
 async function run() {
   try {
     await client.connect();
@@ -31,25 +54,13 @@ async function run() {
       res.send(result);
     });
 
-    app.get(
-      "/destinations/:id",
-      (req, res, next) => {
-        const header = req.headers.authorization;
-        console.log(header);
-        if (header === "logged in") {
-          next();
-        } else {
-          res.status(401).json({ message: "Unauthorized" });
-        }
-      },
-      async (req, res) => {
-        const { id } = req.params;
-        const result = await destinationCollection.findOne({
-          _id: new ObjectId(id),
-        });
-        res.send(result);
-      },
-    );
+    app.get("/destinations/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
 
     app.patch("/destinations/:id", async (req, res) => {
       const { id } = req.params;
@@ -76,19 +87,19 @@ async function run() {
       res.json(result);
     });
 
-    app.get("/booking/:userId", async (req, res) => {
+    app.get("/booking/:userId", verifyToken, async (req, res) => {
       const { userId } = req.params;
       const result = await bookingCollection.find({ userId: userId }).toArray();
       res.json(result);
     });
 
-    app.post("/booking", async (req, res) => {
+    app.post("/booking", verifyToken, async (req, res) => {
       const bookingData = req.body;
       const result = await bookingCollection.insertOne(bookingData);
       res.json(result);
     });
 
-    app.delete("/booking/:bookingId", async (req, res) => {
+    app.delete("/booking/:bookingId", verifyToken, async (req, res) => {
       const { bookingId } = req.params;
       const result = await bookingCollection.deleteOne({
         _id: new ObjectId(bookingId),
